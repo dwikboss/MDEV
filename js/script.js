ff324c5b03e3d43003f1e55dfd92e8a00c3279bf
Dwiky van Bosstraten <dwikboss99@gmail.com>
2022-11-02 16:10:14 +0100

    files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+//a = 1;  // This will cause an error (a is not defined).
+
+console.log('TEST: link index.html and script.js');
+
+if (navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.register('../sw.js');
+}
---
+++ sw.js
@@ -0,0 +1,4 @@
+"use strict";
+//a = 1;  // This will cause an error (a is not defined).
+
+console.log("TEST: link script.js and sw.js")
