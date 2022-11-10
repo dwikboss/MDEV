486fe6beae3663d63b1954e48cd10ebe8d2edf9d
Dwiky van Bosstraten <dwikboss99@gmail.com>
2022-11-10 21:39:13 +0100

    finished sw and added geo location

--- js/script.js
+++ js/script.js
@@ -4,10 +4,21 @@
 console.log('TEST: link index.html and script.js');
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register('../sw.js').then(registration => {
+	navigator.serviceWorker.register('sw.js').then(registration => {
 		console.log(registration);
 	}).catch(error => {
 		console.log(error);
 	});
 }
 
+if ("geolocation" in navigator) {
+	navigator.geolocation.watchPosition((position) => {
+		console.log(position.coords.latitude, position.coords.longitude);
+	});
+}
+
+if ("BarcodeDetector" in window ) {
+	console.log('Barcode Detector supported!');
+} else {
+	console.log('not supported');
+}
--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 self.addEventListener("install", e => {
     console.log("install");
+    e.waitUntil(
+        caches.open("shell").then(cache => {
+            return cache.addAll(["./", "./css/style.css", "./css/fonts/Poppins-Bold.ttf", "./css/fonts/Poppins-ExtraBold.ttf", "./css/fonts/Poppins-Light.ttf", "./css/fonts/Poppins-Regular.ttf", "./icon/icon-192.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
 });
