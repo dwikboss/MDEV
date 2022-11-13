fa8f3e70301276dde9e7395adf8c2af6a6df22d3
Dwiky van Bosstraten <dwikboss99@gmail.com>
2022-11-13 23:33:08 +0100

    added boarding pass

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@
 
 console.log('TEST: link index.html and script.js');
 
-// nav
+
+var title = document.getElementById("title");
+
 var nav_news = document.getElementById("nav_news");
 var nav_flights = document.getElementById("nav_flights");
 var nav_contact = document.getElementById("nav_contact");
+
+var upgrade_1 = document.getElementById("card_1_upgrade");
+var card_2 = document.getElementById("card_2");
+var card_3 = document.getElementById("card_3");
 
 var section_news = document.getElementById("section_news");
 var section_flights = document.getElementById("section_flights");
@@ -15,13 +21,21 @@ nav_news.addEventListener("click", handleNav);
 nav_flights.addEventListener("click", handleNav);
 nav_contact.addEventListener("click", handleNav);
 
+upgrade_1.addEventListener("click", handleUpgrade);
+
+function handleUpgrade() {
+	console.log(this.id);
+}
+
 function handleNav() {
 	if (this.id == "nav_news") {
 		section_news.style.display = "flex";
 		section_flights.style.display = "none";
+		title.innerHTML = "updates";
 	} else if (this.id == "nav_flights") {
 		section_news.style.display = "none";
-		section_flights.style.display = "flex";
+		section_flights.style.display = "flex";;
+		title.innerHTML = "flights";
 	} else {
 		console.log("333");
 	}
