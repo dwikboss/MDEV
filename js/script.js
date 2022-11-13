f1896080e1572e1ed328e09f10fef5957d992251
Dwiky van Bosstraten <dwikboss99@gmail.com>
2022-11-13 21:06:43 +0100

    added new cards

--- js/script.js
+++ js/script.js
@@ -3,6 +3,30 @@
 
 console.log('TEST: link index.html and script.js');
 
+// nav
+var nav_news = document.getElementById("nav_news");
+var nav_music = document.getElementById("nav_music");
+var nav_map = document.getElementById("nav_map");
+
+var news_section = document.getElementById("news_section");
+var show_section = document.getElementById("show_section");
+
+nav_news.addEventListener("click", handleNav);
+nav_music.addEventListener("click", handleNav);
+nav_map.addEventListener("click", handleNav);
+
+function handleNav() {
+	if (this.id == "nav_news") {
+		news_section.style.display = "flex";
+		show_section.style.display = "none";
+	} else if (this.id == "nav_music") {
+		news_section.style.display = "none";
+		show_section.style.display = "flex";
+	} else {
+		console.log("333");
+	}
+}
+
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register('sw.js').then(registration => {
 		console.log(registration);
